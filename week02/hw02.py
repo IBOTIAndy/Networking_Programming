9d448c88306c070f29782dfcc3704d9de6a87239
IBOTIAndy <visua64@gmail.com>
2020-10-17 04:36:00 -0700

    code form the TA

--- week02/hw02.py
+++ week02/hw02.py
@@ -1,4 +1,43 @@
 import requests
+import re
+import json
+#import bs4
+#from bs4 import BeautifulSoup
+#帳號密碼登入
+url = "https://irs.zuvio.com.tw/irs/submitLogin" #網址
+account = {"email":"xxxxxx@gmail.com", "password":"your_pass"} #帳號密碼
+r = requests.post(url, data=account) #使用post取得網頁資料
+r.encoding = "utf-8"
+#print("r.text:\n", r.text, "\n r  end")
+#使用Token來找使用者的任何資料 
+#如果自己寫網頁 token最好每次都改(zuvio不會更換)(如依據時間回傳不同的token)
+accessToken = re.search("accessToken = \"[0-9a-f]+\"", r.text).group() #搜尋存取碼, accessToken會有雙引號(")
+#使用正規表達式, group回傳所有小組的字串
+user_id = re.search("user_id = [0-9]+", r.text).group()
+#print(re.search("user_id = [0-9]+", r.text))
+#print(user_id)
+url = "https://irs.zuvio.com.tw/course/listStudentFullCourses?" + user_id + "&" + accessToken
+#print(url)
+url = url.replace(' ', '').replace('"', '') #清除空格與雙引號
+#print(url)
+g = requests.get(url)
+#print(g.text)
+semesters = json.loads(g.text)['semesters']
+for sem in semesters:
+    for cou in sem['courses']:
+        print(cou['course_name'])
+'''
+if(r.status_code == 200): #檢查網頁是否正常擷取
+    print("url \"" + url + "\" is work.")
+print(r.headers['content-type'])
+r.encoding = "utf-8" #使用utf-8編碼讀取
+print("paper encode:", r.encoding)
+#print("paper text:\n", r.text)
+bs_r = bs4.BeautifulSoup(r.text, "html.parser")
+'''
+
+'''
+import requests
 import bs4
 #from bs4 import BeautifulSoup
 #帳號密碼登入
@@ -29,3 +68,4 @@ bs_r = bs4.BeautifulSoup(r.text, "html.parser")
 #    f.write(test.text)
 #with open("myFile.txt", 'r', encoding="utf-8") as f:
 #    print("file:\n", f.read())
+'''
